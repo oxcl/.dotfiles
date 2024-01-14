25827e5f49a05609400213b1be2235bfee6a146e
oxcl <oxcl@proton.me>
2024-01-14 14:33:36 +0330

    fix emacs auto-inserts

--- home/.config/emacs/auto-inserts/_.c
+++ home/.config/emacs/auto-inserts/_.c
@@ -1,4 +1,5 @@
-`(let ((header-file (expand-file-name (concat (file-name-base (buffer-file-name)) ".h") (file-name-directory (buffer-file-name)))))
+`(let ((description "this auto-insert checks if there is a header file with the same name in the same directory of this file and automatically adds the #include macro for it")
+       (header-file (expand-file-name (concat (file-name-base (buffer-file-name)) ".h") (file-name-directory (buffer-file-name)))))
     (when (file-exists-p header-file) (concat "#include \"" (file-name-nondirectory header-file) "\"\n")))`
 $0
 
