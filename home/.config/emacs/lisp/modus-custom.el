d803426e2f73a77615304f801fc5614cdba9e341
oxcl <oxcl@proton.me>
2024-05-27 16:48:22 +0330

    modify emacs config

---
+++ home/.config/emacs/lisp/modus-custom.el
@@ -0,0 +1,5 @@
+(provide 'modus-custom)
+
+;; this is my custom theme using the modus-themes package as a framework for coloring emacs
+;; the colors are automatically injeceted in this file using the 'pelz' script i wrote which you can find
+;; in my dotfilese (~/.local/bin/pelz).
