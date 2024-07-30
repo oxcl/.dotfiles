c99eec245c5238e051a3090f19639f5f967ba32c
oxcl <oxcl@proton.me>
2024-07-30 14:51:59 +0330

    add bootstrap.sh for bootstrapping the dotfiles for the first time on a new machine

---
+++ bootstrap.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# this script is supposed to setup the dotfiles on a new system. run it with "bash bootstrap.sh" and everything should be setted up for you
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+source $SCRIPT_DIR/.local/bin/stowhome
