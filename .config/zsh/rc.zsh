f9dcb1cd23042e789156ea64b19f13e8db743d22
oxcl <oxcl@proton.me>
2024-07-30 16:53:08 +0330

    add ohmyzsh and fast-syntax-highlighting to zsh

--- .config/zsh/rc.zsh
+++ .config/zsh/rc.zsh
@@ -13,24 +13,30 @@ if [[ -r "${XDG_CACHE_HOME}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   source "${XDG_CACHE_HOME}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
-
-
+####################
+# PLUGINS
+####################
+# simple function to load zsh plugins and scripts
+# i use git submodules and this function as my zsh plugin manager
 function load(){
   local PLUGIN="$1"
-  if [[ ! -d "$HERE/plugins/$PLUGIN" ]]; then
-    echo "failed to load plugin $PLUGIN: directory $HERE/plugins/$PLUGIN does not exist" >&2
+  if [[ ! -d "$HERE/plugins/$PLUGIN" ]] && [[ ! -f "$HERE/plugins/$PLUGIN.zsh" ]]; then
+    echo "failed to load plugin $PLUGIN: directory or file $HERE/plugins/$PLUGIN does not exist" >&2
     return -1
   fi
 
   function try(){
     if [[ -f "$1" ]]; then source "$1"; else return 1; fi
   }
+  try "$HERE/plugins/$PLUGIN.zsh" || \
   try "$HERE/plugins/$PLUGIN/$PLUGIN.zsh-theme" || \
   try "$HERE/plugins/$PLUGIN/$PLUGIN.plugin.zsh" || \
+  try "$HERE/plugins/$PLUGIN/$PLUGIN.zsh" || \
   echo "failed to load plugin $PLUGIN: don't know which file to source" >&2
 }
 
-load powerlevel10k
+load powerlevel10k # zsh prompt
+load fast-syntax-highlighting # syntax highlighting for commands
 
 
 [[ -f "$HERE/p10k.zsh" ]] && source "$HERE/p10k.zsh"
