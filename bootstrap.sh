49f8b80c792520ad35ca295571b03bb8e3bc080e
oxcl <oxcl@proton.me>
2024-07-31 08:42:03 +0330

    improve bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # this script is supposed to setup the dotfiles on a new system. run it with "bash bootstrap.sh" and everything should be setted up for you
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
-git submodule update --init --recursive
-source $SCRIPT_DIR/.local/bin/stowhome
+git -C "$SCRIPT_DIR" submodule update --init --recursive
+bash "$SCRIPT_DIR/.local/bin/stowhome"
