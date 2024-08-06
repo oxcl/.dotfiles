019a28028271306949079dea1cfdd8e11dc43267
oxcl <oxcl@proton.me>
2024-08-06 15:12:30 +0330

    add gitignore script

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -24,6 +24,43 @@ alias l.="ls -ldv .*" # only list dotfiles
 alias lt="ls -lth" # sort by time (most recent)
 alias lS="ls -lSh" # sort by size (largest first)
 
+# make grep use colors and ignore some special folders by default
+GREP_OPTIONS="--color=auto --exclude-dir={.git,CVS,.hg,.bzr,.svn,.idea,.tox}"
+alias grep="grep $GREP_OPTIONS"
+alias egrep="egrep $GREP_OPTIONS"
+alias fgrep="fgrep $FREP_OPTIONS"
+unset GREP_OPTIONS
+
+# aliases for find
+alias findf="find . -type f -name" # search through files
+alias findd="find . -type d -name" # search through directories
+
+
+# don't add unnecessary commands to history. for this to work HIST_NO_SPACE
+# must be set.
+alias exit=" exit"
+
+# human readable file sizes
+alias df="df -h"
+alias du="du -h"
+
+
+# pretty print json from stdin
+alias json="jq . --color-output"
+
+# python aliases
+command -v python3 &>/dev/null && alias python="python3"
+command -v pip3 &>/dev/null && alias pip="pip3"
+
+if command -v python3 &>/dev/null; then
+  alias venv="python3 -m venv"
+elif command -v python &>/dev/null; then
+  alias venv="python -m venv"
+fi
+
+# breaking the habbit of using clear instead of Ctrl-l
+alias clear=" echo 'use Ctrl-l'"
+
 # use bat instead of cat if possible
 replace cat bat
 # use bat to show pretified --help output for every command
