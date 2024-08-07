b0c29fe177b37279e7209341771725d000d676a3
oxcl <oxcl@proton.me>
2024-08-07 14:21:17 +0330

    install firefox

--- old/custom/nix.plugin.zsh
+++
@@ -1,7 +0,0 @@
-#/usr/bin/env zsh
-# helper functions to make working with nix/nixos easier
-
-# recursive symlink lookup for binaries inside $PATH since most binaries in nix/nixos are symlinks
-function whichnix(){ readlink -f "$(command which $1)"; }
-function wherenix(){ readlink -f "$(command where $1)"; }
-alias which=whichnix
