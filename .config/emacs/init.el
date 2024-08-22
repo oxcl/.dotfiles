4ce4944aa02540096b0dab4cc2c0b026108040a6
oxcl <oxcl@proton.me>
2024-08-22 13:58:05 +0330

    swap C-g and ESC in gui and terminal

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,3 +1,31 @@
+(use-package emacs
+  :config
+  ;; the following code snippet is used to enable the <escape> key to work in terminal environments
+  ;; taken from: https://github.com/emacsorphanage/god-mode/issues/43#issuecomment-67193877
+  (defvar oxcl/fast-keyseq-timeout 50) ; this timeout determines if a ESC is a Alt combination or a single <escape>
+  
+  (defun oxcl/-tty-ESC-filter (map)
+    (if (and (equal (this-single-command-keys) [?\e])
+             (sit-for (/ oxcl/fast-keyseq-timeout 1000.0)))
+        [?\C-g] map)) ; i replaced [escape] with [?\C-g] because i want to swap them
+  
+  (defun oxcl/-lookup-key (map key)
+    (catch 'found
+      (map-keymap (lambda (k b) (if (equal key k) (throw 'found b))) map)))
+  
+  (defun oxcl/catch-tty-ESC ()
+    "Setup key mappings of current terminal to turn a tty's ESC into `escape'."
+    (when (memq (terminal-live-p (frame-terminal)) '(t pc))
+      (let ((esc-binding (oxcl/-lookup-key input-decode-map ?\e)))
+        (define-key input-decode-map
+          [?\e] `(menu-item "" ,esc-binding :filter oxcl/-tty-ESC-filter)))))  
+  (oxcl/catch-tty-ESC)
+
+  ;; swap <escape> and Ctrl-g because Ctrl-g is just too good to just quit
+  ;; so in key bindings <escape> is actually C-g
+  (define-key input-decode-map [?\C-g] [escape] )
+  (define-key input-decode-map [escape] [?\C-g] ))
+
 (use-package org
   :custom
   ;; virtually indent headlines and their contents
@@ -14,5 +42,10 @@
 		       (,(kbd "S-<left>")    . ,(kbd "M-,"))
 		       (,(kbd "C-S-<right>") . ,(kbd "C-M-."))
 		       (,(kbd "C-S-<left>")  . ,(kbd "C-M-,"))))
+  ;; don't remove highlighted matches when searching with org-occur
+  (org-remove-highlights-with-change nil)
+  :bind (:map org-mode-map
+	      ("<escape>" . next-error)
+	      ("C-S-g"    . previous-error))
   :commands (org-mode))
   
